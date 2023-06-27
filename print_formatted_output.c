5f861e96702188c2dac183debe27890b44299e79
SundayMba <sundinoh@gmail.com>
2023-06-27 11:46:53 +0100

    null 1

--- print_formatted_output.c
+++ print_formatted_output.c
@@ -22,7 +22,7 @@ int replace_char(va_list ap_list, char id)
 	for (i = 0; fmt_op[i].c != '\0'; i++)
 		if (fmt_op[i].c == id)
 			return (fmt_op[i].write_func(ap_list));
-	return (0);
+	return (-1);
 }
 
 
